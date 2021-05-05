86feb0c0667646e048acdb98b1127d2c22f280ec
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-05 22:20:34 +0700

    prime

---
+++ Week_1/Loop in Java/PrimeNumber/src/PrimeNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number=sc.nextInt();
+        if(number<2) {
+            System.out.println(number+" is not a prime!");
+        } else {
+            int i=2;
+            boolean isPrime=true;
+            while (i<=Math.sqrt(number)) {
+                if(number%i==0) {
+                    isPrime=false;
+                    break;
+                }
+                i++;
+            }
+            if(isPrime)
+                System.out.println(number+" is a prime!");
+            else
+                System.out.println(number+" is not a prime!");
+        }
+    }
+}
