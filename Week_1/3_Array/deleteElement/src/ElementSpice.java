6b7133de1850999427a4da550326e35b5cb658f0
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-06 22:46:10 +0700

    delete element in array

---
+++ Week_1/3_Array/deleteElement/src/ElementSpice.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class ElementSpice {
+    public static void main(String[] args) {
+        //init array
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int size=sc.nextInt();
+        int[]array=new int[size];
+        for(int i=0;i<array.length;i++) {
+            System.out.print("Enter element "+i+": ");
+            array[i]=sc.nextInt();
+        }
+        System.out.printf("%-20s%s","Elements in array: ","");
+        for (int a:array) {
+            System.out.print(a+"\t");
+        }
+        //Enter number to delete
+        System.out.print("\n"+"Enter number to delete: ");
+        int x=sc.nextInt();
+        //Check x is in array
+        int index_del=-1;
+        boolean exist=false;
+        for(int i=0;i<array.length;i++) {
+            if(array[i]==x) {
+                exist=true;
+                index_del=i;
+                break;
+            }
+        }
+        if(!exist) {
+            System.out.println(x+"is not exist in array");
+        }
+
+        //Delete x out array
+        for (int i=index_del;i<array.length-1;i++){
+            array[i]=array[i+1];
+        }
+        array[array.length-1]=0;
+        System.out.printf("%-20s%s","After delete, elements in array: ","");
+        for (int a:array) {
+            System.out.print(a+"\t");
+        }
+    }
+}
