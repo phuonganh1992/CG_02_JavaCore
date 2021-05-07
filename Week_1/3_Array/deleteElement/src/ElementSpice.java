7a97033b71abb53f35d9094e61613a7f2d0253bc
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-07 08:22:36 +0700

    delete element appear many times in array

--- Week_1/3_Array/deleteElement/src/ElementSpice.java
+++ Week_1/3_Array/deleteElement/src/ElementSpice.java
@@ -18,28 +18,34 @@ public class ElementSpice {
         //Enter number to delete
         System.out.print("\n"+"Enter number to delete: ");
         int x=sc.nextInt();
+
         //Check x is in array
-        int index_del=-1;
-        boolean exist=false;
-        for(int i=0;i<array.length;i++) {
-            if(array[i]==x) {
-                exist=true;
-                index_del=i;
-                break;
-            }
-        }
-        if(!exist) {
-            System.out.println(x+"is not exist in array");
-        }
+        int index_del=indexValue(array,x);
+
 
         //Delete x out array
-        for (int i=index_del;i<array.length-1;i++){
-            array[i]=array[i+1];
+        int count=0;
+        while(index_del!=-1) {
+            for (int i = index_del; i < array.length - 1; i++) {
+                array[i] = array[i + 1];
+            }
+            array[array.length - 1-count] = 0;
+            count++;
+            index_del=indexValue(array,x);
         }
-        array[array.length-1]=0;
         System.out.printf("%-20s%s","After delete, elements in array: ","");
         for (int a:array) {
             System.out.print(a+"\t");
         }
+    }
+    public static int indexValue(int[]arr,int x) {
+        int index=-1;
+        for(int i=0;i<arr.length;i++) {
+            if(arr[i]==x) {
+                index=i;
+                break;
+            }
+        }
+        return index;
     }
 }
