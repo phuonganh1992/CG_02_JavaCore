c91e6bbdc854e0a34c2c7f1709cba4cd02370c96
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-10 10:07:53 +0700

    Point 2D & point 3D

---
+++ Week_2/Inheritance/Point2D_Point3D/src/Point2DTest.java
@@ -0,0 +1,8 @@
+public class Point2DTest {
+    public static void main(String[] args) {
+        Point2D a=new Point2D();
+        System.out.println(a);
+        Point2D b=new Point2D(4,5);
+        System.out.println(b);
+    }
+}
---
+++ Week_2/Inheritance/Point2D_Point3D/src/Point3DTest.java
@@ -0,0 +1,10 @@
+public class Point3DTest {
+    public static void main(String[] args) {
+        Point2D m=new Point3D();
+        System.out.println(m);
+
+        Point2D n=new Point3D(3,4,5);
+        System.out.println(n);
+    }
+
+}
