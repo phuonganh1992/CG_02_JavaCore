b185b8a3dd3ceb9e0deba0a0ae004dd2418b938d
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-11 13:37:09 +0700

    color

---
+++ Week_2/Abstract Class, Interface/Colorable/src/ColorableTest.java
@@ -0,0 +1,13 @@
+public class ColorableTest {
+    public static void main(String[] args) {
+        Shape[] shapes=new Shape[5];
+        shapes[0]=new Circle(3);
+        shapes[1]=new Rectangle(4,5);
+        shapes[2]=new Square(6);
+        shapes[3]=new Circle(7);
+        shapes[4]=new Square(8);
+
+    }
+
+
+}
