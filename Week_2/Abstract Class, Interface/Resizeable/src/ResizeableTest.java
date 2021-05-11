e1b0659e974baf8fff8ccbb7c88075bbde0109de
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-11 14:18:07 +0700

    fix Resizeable

--- Week_2/Abstract Class, Interface/Resizeable/src/ResizeableTest.java
+++ Week_2/Abstract Class, Interface/Resizeable/src/ResizeableTest.java
@@ -4,11 +4,20 @@ public class ResizeableTest {
         shapes[0]=new Circle(4);
         shapes[1]=new Rectangle(5,6);
         shapes[2]=new Square(7);
+        System.out.println("Before resize: ");
+
         for(Shape shape:shapes){
             System.out.println(shape);
-            if(shape instanceof Square){
-                Resizeable rs=(Square)shape;
-                rs.resize(3);
+        }
+
+        System.out.println("Afer resize: ");
+        for(Shape shape:shapes){
+            double percent=Math.round(Math.random()*100+1);
+
+            if(shape instanceof Resizeable){
+                System.out.print("Percent is: ");
+                System.out.println(percent);
+                ((Resizeable) shape).resize(percent);
                 System.out.println(shape);
             }
         }
