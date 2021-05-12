28d486ff9814d1ed943c1f7243f8da0ff87851ac
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-12 08:50:46 +0700

    caculator

--- Week_2/Clean code/01_PracticeRefactoring/src/Calculator.java
+++ Week_2/Clean code/01_PracticeRefactoring/src/Calculator.java
@@ -1,8 +1,8 @@
 public class Calculator {
-    public static final char ADDITION="+";
-    public static final char SUBTRACTION="-";
-    public static final char MULTIPLICATION="*";
-    public static final char DIVISION="/";
+    public static final char ADDITION='+';
+    public static final char SUBTRACTION='-';
+    public static final char MULTIPLICATION='*';
+    public static final char DIVISION='/';
 
     public static int calculate(int firstOperand, int secondOperand, char operator) {
         switch (operator) {
