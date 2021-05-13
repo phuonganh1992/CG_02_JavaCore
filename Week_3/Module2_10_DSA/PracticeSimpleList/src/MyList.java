b4cf2015411816da796f2b77271d7bea8d67101e
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-13 17:16:00 +0700

    .

---
+++ Week_3/Module2_10_DSA/PracticeSimpleList/src/MyList.java
@@ -0,0 +1,17 @@
+public class MyList {
+    private int size=0;
+    private int DEFAULT_CAPACITY=10;
+    private Object elements[];
+
+    public MyList() {
+    }
+    public void add(){
+
+    }
+    public int ensureCapa(){
+        return 0;
+    }
+    public void get(){
+
+    }
+}
