fc208d63a6e3c2c468c52a7c50a5ebbf503e443c
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-14 10:32:21 +0700

    -

--- Week_3/Module2_10_DSA/MethodArrayList/src/MyListTest.java
+++ Week_3/Module2_10_DSA/MethodArrayList/src/MyListTest.java
@@ -3,15 +3,13 @@ import java.util.Arrays;
 public class MyListTest {
     public static void main(String[] args) {
         MyList<Integer> listInteger=new MyList<Integer>(12);
+
         System.out.println("Số phần tử là: ");
         System.out.println(listInteger.size());
         listInteger.add(3,12);
 
         System.out.println("Số phần tử là: ");
         System.out.println(listInteger.size());
-
-
-
 
     }
 
