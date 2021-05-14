04c14785557759a2a362d92a67cf216a17aaa995
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-14 14:01:44 +0700

    done xay dung array list

--- Week_3/Module2_10_DSA/MethodArrayList/src/MyList.java
+++ Week_3/Module2_10_DSA/MethodArrayList/src/MyList.java
@@ -2,104 +2,116 @@
 import java.util.Arrays;
 
 public class MyList<E> {
-    public int size=0;
-    public static final int DEFAULT_CAPACITY=10;
+    public int size = 0;
+    public static final int DEFAULT_CAPACITY = 10;
     public Object[] elements;
 
     public MyList() {
-        elements=new Object[DEFAULT_CAPACITY];
+        elements = new Object[DEFAULT_CAPACITY];
     }
 
     public MyList(int capacity) {
-        if (capacity>0){
-            elements=new Object[capacity];
-        } else if (capacity==0){
-            elements=new Object[]{};
-        } else throw new IllegalArgumentException("Illegal capacity: "+capacity);
+        if (capacity > 0) {
+            elements = new Object[capacity];
+        } else if (capacity == 0) {
+            elements = new Object[]{};
+        } else throw new IllegalArgumentException("Illegal capacity: " + capacity);
     }
-    public void print(){
+
+    public void print() {
         System.out.println("My list is: ");
         for (int i = 0; i < elements.length; i++) {
-            System.out.print(elements[i]+", ");
+            System.out.print(elements[i] + ", ");
         }
         System.out.println();
     }
-    public void add(int index,E element){
-        if (index<0 || index>size) {
+
+    public void add(int index, E element) {
+        if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException("Index is out of range");
-        } else if(size==elements.length) {
-                elements = Arrays.copyOf(elements, size + 1);
-                size++;
+        } else if (size == elements.length) {
+            elements = Arrays.copyOf(elements, size + 1);
+            size++;
         }
 //        else if(index==size && size==0) {
 //                size=size+1;
 //                elements[index]=element;
 //            }
         else {
-                for (int i = size; i > index; i--) {
-                    elements[i] = elements[i - 1];
-                }
-                elements[index] = element;
-                size++;
+            for (int i = size; i > index; i--) {
+                elements[i] = elements[i - 1];
             }
-    }
-    public E remove(int index){
-        if (index<0 || index>=size) throw new IndexOutOfBoundsException("Index is out of range");
-        E oldValue=(E) elements[index];
-        for (int i = index; i <size-1 ; i++) {
-                elements[i] = elements[i + 1];
+            elements[index] = element;
+            size++;
         }
-        elements[size-1]=null;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Index is out of range");
+        E oldValue = (E) elements[index];
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        elements[size - 1] = null;
         size--;
         return oldValue;
     }
-    public int size(){
+
+    public int size() {
         return size;
     }
-    public void setSize(int size){
-        this.size=size;
+
+    public void setSize(int size) {
+        this.size = size;
     }
-    public E[] clone(){
-        return (E[]) elements;
+
+    public MyList<E> clone() {
+        return this;
     }
-    public boolean contains(E o){
-        boolean isContains=false;
+
+    public boolean contains(E o) {
+        boolean isContains = false;
         for (int i = 0; i < size; i++) {
-            if(elements[i]==o){
-                isContains=true;
+            if (elements[i] == o) {
+                isContains = true;
             }
         }
         return isContains;
     }
-    public int indexOf(E o){
-        int index=-1;
-        for (int i=0; i < size; i++) {
-            if(elements[i]==o) {
-                index=i;
+
+    public int indexOf(E o) {
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if (elements[i] == o) {
+                index = i;
                 break;
             }
         }
         return index;
     }
-    public boolean add(E e){
-        if(size==0) return false;
+
+    public boolean add(E e) {
+        if (size == 0) return false;
         return true;
     }
-    public void ensureCapacity(int minCapacity){
-        if(minCapacity> elements.length) {
-            elements=Arrays.copyOf(elements,minCapacity);
+
+    public void ensureCapacity(int minCapacity) {
+        if (minCapacity > elements.length) {
+            elements = Arrays.copyOf(elements, minCapacity);
         }
     }
-    public E get(int i){
-        if(i<0 || i>=size){
-            throw new IndexOutOfBoundsException("Index "+i+" is out of size");
+
+    public E get(int i) {
+        if (i < 0 || i >= size) {
+            throw new IndexOutOfBoundsException("Index " + i + " is out of size");
         }
         return (E) elements[i];
     }
-    public void clear(){
+
+    public void clear() {
         for (int i = 0; i < size; i++) {
-            elements[i]=null;
+            elements[i] = null;
         }
-        size=0;
+        size = 0;
     }
 }
--- Week_3/Module2_10_DSA/MethodArrayList/src/MyListTest.java
+++ Week_3/Module2_10_DSA/MethodArrayList/src/MyListTest.java
@@ -5,36 +5,40 @@ public class MyListTest {
         MyList<Integer> listInteger=new MyList<Integer>(15);
         System.out.println("Capacity is: "+listInteger.elements.length);
         System.out.println("Size is: "+listInteger.size());
-        listInteger.setSize(4);
+//        listInteger.setSize(4);
         listInteger.print();
         listInteger.add(0,12);
-        listInteger.add(2,15);
-        listInteger.add(3,17);
-        listInteger.add(4,19);
-        listInteger.add(5,21);
-        listInteger.add(6,23);
+        listInteger.add(1,15);
+        listInteger.add(2,17);
+        listInteger.add(3,19);
+        listInteger.add(4,21);
+        listInteger.add(5,23);
         listInteger.print();
         System.out.println("Size after add is: "+listInteger.size());
         System.out.println("Capacity after add is: "+listInteger.elements.length);
 
         System.out.println(listInteger.contains(20));
         System.out.println(listInteger.indexOf(21));
-//        Integer[] copyList;
-//        copyList = listInteger.clone();
-        System.out.println(listInteger.clone());
+        MyList<Integer> copyList= (MyList<Integer>) listInteger.clone();
 
-//        listInteger.remove(5);
-//        listInteger.print();
-//        System.out.println("Size after remove is: "+listInteger.size());
-//        System.out.println("Capacity after remove is: "+listInteger.elements.length);
-//
-//        listInteger.clear();
-//        listInteger.print();
-//        System.out.println("Size after clear is: "+listInteger.size());
-//        System.out.println("Capacity after clear is: "+listInteger.elements.length);
-//
-//        listInteger.ensureCapacity(50);
-//        System.out.println("Capacity is: "+listInteger.elements.length);
+        System.out.println("copy list is: ");
+        for (Object o: copyList.elements
+             ) {
+            System.out.println(o);
+        }
+
+        listInteger.remove(5);
+        listInteger.print();
+        System.out.println("Size after remove is: "+listInteger.size());
+        System.out.println("Capacity after remove is: "+listInteger.elements.length);
+
+        listInteger.clear();
+        listInteger.print();
+        System.out.println("Size after clear is: "+listInteger.size());
+        System.out.println("Capacity after clear is: "+listInteger.elements.length);
+
+        listInteger.ensureCapacity(50);
+        System.out.println("Capacity is: "+listInteger.elements.length);
     }
 
 
