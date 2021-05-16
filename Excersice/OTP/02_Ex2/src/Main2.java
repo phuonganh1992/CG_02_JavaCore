a1a36e9c86e6e7b0d2db9670fdff9aeb9b6f2136
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-16 23:41:41 +0700

    .

--- Excersice/OTP/02_Ex2/src/Main2.java
+++ Excersice/OTP/02_Ex2/src/Main2.java
@@ -4,7 +4,6 @@ public class Main2 {
     public static void main(String[] args) {
         ManageDocument listDocument=new ManageDocument();
         int choice;
-        int size;
         Scanner sc=new Scanner(System.in);
         do{
             menu();
--- Excersice/OTP/03_Ex3/src/Main.java
+++ Excersice/OTP/03_Ex3/src/Main.java
@@ -1,15 +1,81 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Enrollment enrollment=new Enrollment();
-        Candidate a=new CandidateA("code1","huong","hn",1);
-        Candidate b=new CandidateB("code2","hoa","hn",1);
-        Candidate c=new CandidateC("code3","hong","hn",1);
-        enrollment.add(a);
-        enrollment.add(b);
-        enrollment.add(c);
-        enrollment.display();
-        Candidate d=enrollment.find("code3");
-        System.out.println(d);
+//        Candidate a=new CandidateA("code1","huong","hn",1);
+//        Candidate b=new CandidateB("code2","hoa","hn",1);
+//        Candidate c=new CandidateC("code3","hong","hn",1);
+//        enrollment.add(a);
+//        enrollment.add(b);
+//        enrollment.add(c);
+//        enrollment.display();
+//        Candidate d=enrollment.find("code3");
+//        System.out.println(d);
+        int choice;
+        Scanner sc=new Scanner(System.in);
+        do{
+            menu();
+            System.out.print("Enter your choice: ");
+            choice=sc.nextInt();
+            switch (choice){
+                case 1:
+                    switch (chooseType()){
+                        case "a":
+                            System.out.println("Enter information candidate ");
+                            Candidate candidateA=new CandidateA();
+                            candidateA.input();
+                            enrollment.add(candidateA);
+                            break;
+                        case "b":
+                            System.out.println("Enter information candidate ");
+                            Candidate candidateB=new CandidateB();
+                            candidateB.input();
+                            enrollment.add(candidateB);
+                            break;
+                        case "c":
+                            System.out.println("Enter information candidate ");
+                            Candidate candidateC=new CandidateC();
+                            candidateC.input();
+                            enrollment.add(candidateC);
+                            break;
+                    }
+                    break;
+                case 2:
+                    enrollment.display();
+                    break;
+                case 3:
+                    System.out.print("Enter id of candidate: ");
+                    sc.nextLine();
+                    String id=sc.nextLine();
+                    Candidate findCandidate=enrollment.find(id);
+                    System.out.println("The result of search: ");
+                    System.out.println(findCandidate);
+
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+            }
+
+        }while (choice>=1 && choice<=4);
+
 
     }
+    static void menu(){
+        System.out.println("\n========MENU===========");
+        System.out.println("1. Add new candidate");
+        System.out.println("2. Display candidate");
+        System.out.println("3. Find candidate");
+        System.out.println("4. Exit");
+    }
+    static String chooseType(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a: candidate A");
+        System.out.println("Enter b: candidate B");
+        System.out.println("Enter c: candidate C");
+        System.out.print("Enter your type: ");
+        return sc.nextLine();
+    }
+
 }
