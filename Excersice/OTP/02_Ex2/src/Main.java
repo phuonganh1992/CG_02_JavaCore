43a1f69b041254eefa667be125ef80cb1d4f85fb
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-16 11:38:04 +0700

    .

--- Excersice/OTP/02_Ex2/src/Main.java
+++ Excersice/OTP/02_Ex2/src/Main.java
@@ -1,8 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        ManageDocument<Book> listBook=new ManageDocument<>();
-        ManageDocument<Magazine> listMagazine=new ManageDocument<>();
-        ManageDocument<Newspaper> listNewsPaper=new ManageDocument<>();
+        ManageDocument listBook=new ManageDocument();
+        int choice;
+        Scanner sc=new Scanner(System.in);
+        do{
+            menu();
+            System.out.println("Enter your choice");
+            choice=sc.nextInt();
+            switch (choice){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
 
+            }
+
+
+
+        } while (choice>=1 && choice<=5);
+
+
+    }
+    static void menu(){
+        System.out.println("\n========MENU===========");
+        System.out.println("1. Add new document");
+        System.out.println("2. Delete document");
+        System.out.println("3. Display information");
+        System.out.println("4. Search document");
+        System.out.println("5. Exit");
     }
 }
--- Excersice/OTP/02_Ex2/src/ManageDocument.java
+++ Excersice/OTP/02_Ex2/src/ManageDocument.java
@@ -1,33 +1,39 @@
 import java.util.ArrayList;
 
-public class ManageDocument<T> {
-    private ArrayList<T> list;
-//    private int size;
+public class ManageDocument {
+    private Document[] listDocument;
+    private int size;
 
     public ManageDocument() {
-        list = new ArrayList<>();
     }
-    public void add(T element){
-        list.add(element);
 
+    public ManageDocument(int size) {
+        listDocument=new Document[size];
     }
-    public void delete(String code){
-        for (T element:list) {
-            if (((Document) element).getCode()==code){
-                list.remove(element);
-            }
-        }
+
+    public void add(Document element){
+        listDocument[size]=element;
+        size++;
     }
-    public void displayDocument(){
-        for (T element:list) {
-            System.out.println(element+"\t");
-        }
-    }
-    public int find(T element){
-        for (int i = 0; i < list.size(); i++) {
-            if (element==list.get(i)) return i;
+
+    public int find(Document element){
+        for (int i = 0; i < size; i++) {
+            if (element==listDocument[i]) return i;
             break;
         }
         return -1;
     }
+    public void delete(String code){
+        for (Document element:listDocument) {
+            if (element.getCode()==code){
+
+            }
+        }
+    }
+    public void displayDocument(){
+        for (Document element:listDocument) {
+            System.out.println(element+"\t");
+        }
+    }
+
 }
