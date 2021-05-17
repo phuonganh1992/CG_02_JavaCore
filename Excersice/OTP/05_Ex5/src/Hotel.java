9e21ee547b229b0ab322de9df2866f879fab0e26
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-17 14:14:55 +0700

    done bai 5

--- Excersice/OTP/05_Ex5/src/Hotel.java
+++ Excersice/OTP/05_Ex5/src/Hotel.java
@@ -5,7 +5,15 @@ public class Hotel {
     private int size;
 
     public Hotel() {
+        people=new Person[100];
+    }
 
+    public Person[] getPeople() {
+        return people;
+    }
+
+    public void setPeople(Person[] people) {
+        this.people = people;
     }
 
     public int getSize() {
@@ -27,12 +35,12 @@ public class Hotel {
             people[i]=person;
         }
     }
+
     public void add(Person person){
-//        Person person=new Person();
-//        person.input();
         people[size]=person;
         size++;
     }
+
     public int find(String id){
         for (int i = 0; i < size; i++) {
             if(people[i].getId().equals(id)) return i;
@@ -43,7 +51,6 @@ public class Hotel {
     public void delete(String id){
         int index=find(id);
         if(index!=-1){
-
             for (int i = index; i < size-1; i++) {
                 people[i]=people[i+1];
             }
--- Excersice/OTP/05_Ex5/src/Main.java
+++ Excersice/OTP/05_Ex5/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Hotel hotel=new Hotel();
         int choice;
+        int index;
+        String id;
         Scanner sc=new Scanner(System.in);
         do{
             menu();
@@ -11,32 +13,43 @@ public class Main {
             choice=sc.nextInt();
             switch (choice){
                 case 1:
-                    hotel.add();
+                    Person person=new Person();
+                    person.input();
+
+                    hotel.add(person);
                     break;
                 case 2:
                     System.out.print("Enter id of client to delete: ");
                     sc.nextLine();
-                    String idDelete=sc.nextLine();
-                    int index=hotel.find(idDelete);
-                    if(index!=-1) hotel.delete(idDelete);
+                    id=sc.nextLine();
+                    index=hotel.find(id);
+                    if(index!=-1) hotel.delete(id);
                     else System.out.println("No client with id above, pls re-input");
                     break;
                 case 3:
                     hotel.display();
                     break;
                 case 4:
-
+                    System.out.print("Enter id of client to search: ");
+                    sc.nextLine();
+                    id=sc.nextLine();
+                    index=hotel.find(id);
+                    if(index==-1) System.out.println("No client with id above, pls re-input");
+                    else System.out.println(hotel.getPeople()[index]);
                     break;
                 case 5:
+                    System.out.print("Enter id of client to calculate the rental: ");
+                    sc.nextLine();
+                    id=sc.nextLine();
+                    index=hotel.find(id);
+                    if(index==-1) System.out.println("No client with id above, pls re-input");
+                    else System.out.println("Rental is "+hotel.calculateRent(id));
                     break;
                 case 6:
                     System.exit(0);
                     break;
-
             }
-        }while (choice>=1 && choice<=5)
-
-
+        }while (choice>=1 && choice<=5);
     }
     static void menu(){
         System.out.println("\n========MENU===========");
@@ -47,6 +60,4 @@ public class Main {
         System.out.println("5. Calculate rent");
         System.out.println("6. Exit");
     }
-
-
 }
