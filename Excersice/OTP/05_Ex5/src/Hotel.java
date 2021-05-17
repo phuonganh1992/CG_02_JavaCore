db13ed3d50b6a9b9913f05769b5626d7443f9114
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-17 08:47:36 +0700

    .

--- Excersice/OTP/05_Ex5/src/Hotel.java
+++ Excersice/OTP/05_Ex5/src/Hotel.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Hotel {
     private Person[] people;
     private int size;
+
+    public Hotel() {
+
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+    public void input() {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter size: ");
+        this.size = sc.nextInt();
+        people=new Person[size];
+        for(int i=0;i<size;i++){
+            System.out.println("Information of person "+(i+1));
+            Person person=new Person();
+            person.input();
+            people[i]=person;
+        }
+    }
+    public int calculateRent(Person person){
+    }
 
 }
