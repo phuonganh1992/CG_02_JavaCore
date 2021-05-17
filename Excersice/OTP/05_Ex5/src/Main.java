dbb0c6a17ea1e7dea59a1e7d7d2bcbf76ac5d317
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-17 12:10:32 +0700

    .

--- Excersice/OTP/05_Ex5/src/Main.java
+++ Excersice/OTP/05_Ex5/src/Main.java
@@ -1,8 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Hotel hotel=new Hotel();
+        int choice;
+        Scanner sc=new Scanner(System.in);
+        do{
+            menu();
+            System.out.print("Enter your choice: ");
+            choice=sc.nextInt();
+            switch (choice){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    System.exit(0);
+                    break;
+
+            }
+        }while (choice>=1 && choice<=5)
 
 
+    }
+    static void menu(){
+        System.out.println("\n========MENU===========");
+        System.out.println("1. Add new client");
+        System.out.println("2. Delete delete client");
+        System.out.println("3. Display all client in hotel");
+        System.out.println("4. Search client");
+        System.out.println("5. Calculate rent");
+        System.out.println("6. Exit");
     }
 
 
