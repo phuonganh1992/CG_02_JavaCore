9102b8ec630cd670b7d0aac4d7096c0f006bfddc
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-17 18:47:02 +0700

    .

---
+++ Excersice/OTP/10_Ex10/src/Document.java
@@ -0,0 +1,15 @@
+public class Document {
+    private String st;
+
+    public Document() {
+    }
+
+    public Document(String st) {
+        this.st = st;
+    }
+    public int countWord(String st){
+        int count=0;
+        st.split("");
+        return count;
+    }
+}
