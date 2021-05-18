3f89a2c87e08b75a61db1514800acae1a30337a9
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-18 09:16:26 +0700

    done use stack

---
+++ Week_3/Module2_11_DSA_Stack,Queue/03_Excercise_UseStack/src/StackReverse.java
@@ -0,0 +1,56 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.Stack;
+
+public class StackReverse {
+    public static void main(String[] args) {
+        System.out.println("-----------------------------INTEGER-----------------------------------");
+        Stack<Integer> stackInteger = new Stack<>();
+        int[] arrays = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("Original array is: ");
+        for (int element:arrays) {
+            System.out.print(element+"\t");
+        }
+        System.out.println();
+        for (int i = 0; i < arrays.length; i++) {
+            stackInteger.push(arrays[i]);
+        }
+        System.out.println("Stack is: ");
+        for (Integer i : stackInteger) {
+            System.out.print(i+"\t");
+        }
+        System.out.println();
+        System.out.println("Use pop to get element out of stack and put in to array: ");
+
+        for (int element:arrays) {
+            while ((!stackInteger.isEmpty())){
+                element=stackInteger.pop();
+                System.out.print(element+"\t");
+            }
+        }
+        System.out.println();
+        System.out.println("----------------------------STRING---------------------------------");
+        Stack<String> stackString=new Stack<>();
+        String st="Em co hai con vit";
+        String[] stword=st.split(" ");
+        System.out.println("String array is: ");
+        for (String element:stword) {
+            System.out.print(element+",");
+        }
+        System.out.println();
+        System.out.println("stack is: ");
+        for (String element:stword){
+            stackString.push(element);
+        }
+        for (String element:stackString) {
+            System.out.print(element+",");
+        }
+        System.out.println();
+        System.out.println("Use pop to get element out of stack and put in to array: ");
+        for (String element:stword) {
+            while ((!stackString.isEmpty())){
+                element=stackString.pop();
+                System.out.print(element+",");
+            }
+        }
+    }
+}
