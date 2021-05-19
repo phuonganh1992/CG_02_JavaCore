72a64bf47c9acc2ca2af57a1fd58e093de6c591e
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-19 11:49:27 +0700

    .

--- Week_3/Module2_11_DSA_Stack,Queue/09_Optional_Palindrome/src/Palindrome.java
+++ Week_3/Module2_11_DSA_Stack,Queue/09_Optional_Palindrome/src/Palindrome.java
@@ -1,9 +1,11 @@
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
 public class Palindrome {
     public static void main(String[] args) {
-
+        Queue<String> queueString=new LinkedList<String>();
         String palin="Able was I ere I saw Elba";
+
     }
 }
