b30a12f97f98939c1ce9dd5e2d67c831b2899714
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-19 12:17:50 +0700

    .

--- Week_3/Module2_11_DSA_Stack,Queue/09_Optional_Palindrome/src/Palindrome.java
+++ Week_3/Module2_11_DSA_Stack,Queue/09_Optional_Palindrome/src/Palindrome.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
@@ -5,7 +6,20 @@ import java.util.Stack;
 public class Palindrome {
     public static void main(String[] args) {
         Queue<String> queueString=new LinkedList<String>();
-        String palin="Able was I ere I saw Elba";
+        Stack<String> stackString=new Stack<String>();
+        String palindrome="Able was I ere I saw Elba";
+        String [] palindromeArray=palindrome.split("");
+        for (String charPalindrome: palindromeArray) {
+            queueString.add(charPalindrome);
+        }
+        System.out.println(queueString);
+        for (String charPalindrome: palindromeArray) {
+            stackString.add(charPalindrome);
+        }
+        System.out.println(stackString);
+
+
+
 
     }
 }
