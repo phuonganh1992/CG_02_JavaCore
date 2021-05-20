790a624dfbbad71332b86ed7d3f39125379eeabb
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-20 23:11:32 +0700

    done

--- Excersice/OTP/10_Ex10/src/Document.java
+++ Excersice/OTP/10_Ex10/src/Document.java
@@ -7,11 +7,17 @@ public class Document {
     public Document(String st) {
         this.st = st;
     }
+    public void standarlizeString(){
+        this.st=st.trim();
+        while (st.indexOf("  ")!=-1) st=st.replaceAll("  "," ");
+    }
     public int countWord(){
+        standarlizeString();
         String[] word=st.split(" ");
         return word.length;
     }
     public int counChar(){
+        standarlizeString();
         String[] word=st.split(" ");
         int count=0;
         for (int i = 0; i < word.length; i++) {
--- Excersice/OTP/10_Ex10/src/Main.java
+++ Excersice/OTP/10_Ex10/src/Main.java
@@ -1,15 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Document document=new Document("em co mot con meo");
+        Document document=new Document("  em  co mot     con     meo ");
         System.out.println(document.countWord());
         System.out.println(document.counChar());
-//        String st="Em co nhieu con meo";
-//        String[] word=st.split(" ");
-//        int sum=0;
-//        for (int i = 0; i < word.length; i++) {
-//            sum+=word[i].length();
-//
-//        }
-//        System.out.println(sum);
     }
 }
