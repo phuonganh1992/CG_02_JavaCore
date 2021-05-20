e607f8d425dc11326918fb7df1ab728ff7e91c76
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-20 13:56:12 +0700

    Done bai 11

--- Week_3/Module2_11_DSA_Stack,Queue/11_Optional_QueueUseStack/src/Main.java
+++ Week_3/Module2_11_DSA_Stack,Queue/11_Optional_QueueUseStack/src/Main.java
@@ -2,43 +2,39 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        RecordManager recordManager=new RecordManager();
         Queue<Record> maleQueue=new LinkedList<>();
         Queue<Record> femaleQue=new LinkedList<>();
-
-        RecordManager recordManager=new RecordManager();
-
+        List<Record> outputRecord=new LinkedList<>();
+        System.out.println("------------------Record after sorting date of birth");
         Collections.sort(recordManager.records);
 
         for (Record record:recordManager.records) {
             System.out.println(record);
         }
 
-//        System.out.println(recordList);
-//
-//        for (Record record:recordList) {
-//            if (record.isGender()) femaleQue.add(record);
-//            else maleQueue.add(record);
-//        }
-//
-//        List<Record> outputRecord=new LinkedList<>();
-//
-//        while (!femaleQue.isEmpty()){
-//            outputRecord.add(femaleQue.remove());
-//        }
-//
-//        while ((!maleQueue.isEmpty())){
-//            Record maleRecord=maleQueue.remove();
-//            for(int i=0;i<outputRecord.size();i++){
-//                if(outputRecord.get(i).getDateOfBirth()> maleRecord.getDateOfBirth() ){
-//
-//                    outputRecord.add(i,maleRecord);
-//                    break;
-//                }
-//            }
-//        }
-//        for (Record record:outputRecord) {
-//            System.out.println(record);
-//        }
+        for (Record record:recordManager.records) {
+            if (record.isGender()) femaleQue.add(record);
+            else maleQueue.add(record);
+        }
+
+        while (!femaleQue.isEmpty()){
+            outputRecord.add(femaleQue.remove());
+        }
+
+        while ((!maleQueue.isEmpty())){
+            Record maleRecord=maleQueue.remove();
+            for(int i=0;i<outputRecord.size();i++){
+                if(outputRecord.get(i).getDateOfBirth()> maleRecord.getDateOfBirth() ){
+                    outputRecord.add(i,maleRecord);
+                    break;
+                }
+            }
+        }
+        System.out.println("---------------------------Record after merging male & female--------------------------");
+        for (Record record:outputRecord) {
+            System.out.println(record);
+        }
 //        System.out.println(outputRecord);
     }
 }
