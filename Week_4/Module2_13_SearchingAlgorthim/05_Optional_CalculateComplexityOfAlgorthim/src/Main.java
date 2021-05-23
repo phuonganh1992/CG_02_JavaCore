afd9b0a62308c8e13b53beea42eccdea185794b2
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-05-23 17:26:55 +0700

    done bai 5

--- Week_4/Module2_13_SearchingAlgorthim/05_Optional_CalculateComplexityOfAlgorthim/src/Main.java
+++ Week_4/Module2_13_SearchingAlgorthim/05_Optional_CalculateComplexityOfAlgorthim/src/Main.java
@@ -3,22 +3,17 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        String string="abcabcdgabxy";
+        String string="abcabcdgeabxy";
         LinkedList<Character> maxString=new LinkedList<>();
         LinkedList<Character> list=new LinkedList<>();
         for (int i = 0; i < string.length(); i++) {
+            if (list.size() > 1 && string.charAt(i) <= list.getLast() && list.contains(string.charAt(i))) list.clear();
             list.add(string.charAt(i));
-            for (int j = i+1; j <string.length() ; j++) {
-                if(string.charAt(j)>list.getLast())
-                    list.add(string.charAt(j));
-            }
             if(list.size()>maxString.size()){
                 maxString.clear();
                 maxString.addAll(list);
-                list.clear();
             }
         }
-
         System.out.println(maxString);
     }
 }
