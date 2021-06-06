51acad765424ccbc9f4f4800bae078c9376a9d76
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-06-06 11:24:12 +0700

    .

---
+++ Week_5/Module2_19_String/06_CrawlNews/src/CrawlNews.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CrawlNews {
+    public static void main(String[] args) {
+        try {
+            URL url=new URL("https://dantri.com.vn/the-gioi.htm");
+            Scanner scanner=new Scanner(new InputStreamReader(url.openStream()));
+            scanner.useDelimiter("\\Z");
+            String content=scanner.next();
+            scanner.close();
+//            content=content.replaceAll("\\n+","");
+            System.out.println(content);
+//            Pattern pattern=Pattern.compile("title=\"(.*?)");
+//            Matcher matcher=pattern.matcher(content);
+//            while (matcher.find()){
+//                System.out.println(matcher.group(1));
+//            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
