bf0611ad74210a6e93cf059b2556805c22ce4018
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-06-09 14:52:55 +0700

    .

---
+++ Week_5/Module2_23_BehavioralDesignPattern/02_Practice_TemplateMethod/src/Meal.java
@@ -0,0 +1,16 @@
+public abstract class Meal {
+    public final void doMeal(){
+        prepareIngredients();
+        cook();
+        eat();
+        cleanUp();
+    }
+
+    public abstract void prepareIngredients();
+    public abstract void cook();
+    public void eat(){
+        System.out.println("Mm, that's good");
+    }
+    public abstract void cleanUp();
+
+}
