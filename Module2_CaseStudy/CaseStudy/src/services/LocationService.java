0d79cce127e0fb478cce887b153d2bb7fa28e99a
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-06-12 13:10:20 +0700

    .

--- Module2_CaseStudy/CaseStudy/src/services/LocationService.java
+++ Module2_CaseStudy/CaseStudy/src/services/LocationService.java
@@ -1,5 +1,22 @@
 package services;
 
+import file.LocationIO;
+import model.Location;
+
+import java.io.IOException;
+import java.util.List;
+
 public class LocationService {
+    public static final String PATH_FILE_LOCATION = "D:\\Java\\Module2_CaseStudy\\CaseStudy\\src\\file\\DataLocation.csv";
+    private List<Location> locations;
+
+    public LocationService() {
+        try {
+            this.locations = LocationIO.readFromFile(PATH_FILE_LOCATION);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
